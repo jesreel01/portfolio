27e770541131b2317a2d3eda1236bf9ecbb92449
Jesreel John Miole <jesreel@vmi1145661.contaboserver.net>
2023-04-08 08:39:39 +0800

    create button component

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,21 +1,21 @@
-import React from 'react'
-import { clsx } from 'clsx'
+import React from 'react';
+import { clsx } from 'clsx';
 
 type Props = {
-  size?: 'sm' | 'base' | 'lg'
-  children?: React.ReactNode
-}
+  size?: 'sm' | 'base' | 'lg';
+  children?: React.ReactNode;
+};
 
-const sizes = {
-  sm: 'border-2 p-1 border-black rounded',
-  base: 'border-2 p-1 border-black rounded',
-  lg: 'border-2 p-1 border-black rounded',
-}
+const sizesClasses = {
+  sm: 'border-2 h-8 py-0 px-1 border-black rounded',
+  base: 'border-2 p-1 h-10 border-black rounded',
+  lg: 'text-lg border-2 p-2 border-black rounded',
+};
 
 const Button = (props: Props) => {
-  const { size = 'base', children } = props
+  const { size = 'base', children } = props;
 
-  return <button className={clsx(sizes[size])}>{children}</button>
-}
+  return <button className={clsx(sizesClasses[size])}>{children}</button>;
+};
 
-export default Button
+export default Button;
